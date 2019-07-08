8a4635fc4546405819d23fb28e62546afaa405c1
Rubens de França Ferreira <rubens.ferreira@pe.senai.br>
2019-07-08 17:07:23 -0300

    ADJUSTS

--- src/main/java/com/ferreirarubens/accesscontrol/common/config/data/DatabaseConfig.java
+++ src/main/java/com/ferreirarubens/accesscontrol/common/config/data/DatabaseConfig.java
@@ -1,12 +1,6 @@
 package com.ferreirarubens.accesscontrol.common.config.data;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 
 /**
